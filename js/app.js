008183b63422f1db60b672f31e25a8ffb905df86
Olha Maslova <olh.maslova@gmail.com>
2018-09-05 21:31:41 -0400

    Create basic game functionality

--- js/app.js
+++ js/app.js
@@ -1,36 +1,114 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(x,y, speed){
+        //Set image for enemy
+        this.sprite = 'images/enemy-bug.png';
+        //Set initial location
+        this.x = x;
+        this.y = y;
+        //Set the speed
+        this.speed = speed;
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt){
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x +=this.speed * dt;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+        //Reset position if enemeny goes off screen
+        if (this.x > 550){
+            this.x = -100;
+        }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+        // Detect collisions
+        if (player.x < this.x + 60 &&
+            player.x + 30 > this.x &&
+            player.y < this.y + 25 &&
+            player.y + 30 > this.y) {
+                player.sprite = 'images/explosion.png';
+                this.x = -100;
+                setTimeout(function(){
+                    player.sprite = 'images/char-princess-girl.png';
+                    player.x = 200;
+                    player.y = 400;
+                }, 500);
+        }
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
-// a handleInput() method.
+// a handleInput() method. 
 
+class Player {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/char-princess-girl.png';
+    }
+
+    update() { 
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(keyEnter) {
+        switch (keyEnter) {
+            case 'left':
+                this.x -= this.speed;
+                if (this.x < 0) {
+                    this.x = 0;
+                }
+                break;
+            case 'right':
+                this.x += this.speed;
+                if (this.x > 400) {
+                    this.x = 400;
+                }
+                break;
+            case 'up':
+                this.y -= this.speed;
+                if (this.y < 0) {
+                    this.x = 200;
+                    this.y = 400;
+                }
+                break;
+            case 'down':
+                this.y += this.speed;
+                if (this.y > 425) {
+                    this.y = 425;
+                }
+                break;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let allEnemies = [];
+let enemy = new Enemy (-100, 300, 100);
+let enemy1 = new Enemy (-100, 200, 200);
+let enemy2 = new Enemy (-100, 100, 300);
+let enemy3 = new Enemy (-100, 300, 400);
+let enemy4 = new Enemy (-100, 200, 500);
+let enemy5 = new Enemy (-100, 100, 150);
+let enemy6 = new Enemy (-100, 200, 250); 
+allEnemies.push(enemy, enemy6, enemy5, enemy4, enemy3, enemy2, enemy1);
 
+let player  = new Player(200, 400, 50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
